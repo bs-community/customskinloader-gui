2d008a1e42c054aeb2c34c1c6f3dc83d38f96a57
Pig Fang <g-plane@hotmail.com>
2021-03-22 15:10:23 +0800

    remove manual HMR

--- src/index.ts
+++ src/index.ts
@@ -21,11 +21,6 @@ init({
   initialLocale: getLocaleFromNavigator(),
 })
 
-// Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
-// Learn more: https://www.snowpack.dev/#hot-module-replacement
-if (import.meta.hot) {
-  import.meta.hot.accept()
-  import.meta.hot.dispose(() => {
-    app.$destroy()
-  })
-}
+new App({
+  target: document.body,
+})
